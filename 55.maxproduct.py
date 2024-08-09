34aa7c235e9915f065c8477115ef763c2c450994
Anandhakrishnan27 <anandhshiva27@gmail.com>
2024-08-08 23:34:50 -0700

    max product in an array

---
+++ 55.maxproduct.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        maxSum = nums[0]
+        curMin, curMax = nums[0], nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                curMin, curMax = curMax, curMin
+
+            curMax = max(nums[i], curMax * nums[i])
+            curMin = min(nums[i], curMin * nums[i])
+
+            maxSum = max(maxSum, curMax)
+
+        return maxSum
+        
