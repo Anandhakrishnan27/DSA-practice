40b4c88520ccd4d90c3d21e9ee10db70115090b6
Anandhakrishnan27 <anandhshiva27@gmail.com>
2024-08-11 22:52:46 -0700

    dates between days

--- 56.daysbetweenDays.py
+++ 56.daysbetweenDays.py
@@ -1,11 +1,11 @@
-from datetime import datetime as dt
+from datetime import datetime as dts
 class Solution:
     def daysBetweenDates(self, date1: str, date2: str) -> int:
         date1 = date1.split('-')
         date2 = date2.split('-')
         date1 = [int(i) for i in date1]
         date2 = [int(i) for i in date2]
-        m = dt(date1[0], date1[1], date1[2])
-        n = dt(date2[0], date2[1], date2[2])
+        m = dts(date1[0], date1[1], date1[2])
+        n = dts(date2[0], date2[1], date2[2])
         return abs((n-m).days)
         
