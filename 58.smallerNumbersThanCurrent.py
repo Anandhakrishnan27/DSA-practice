afce7ff285bfdc418604e9cc0fe5effa52a8230f
Anandhakrishnan27 <anandhshiva27@gmail.com>
2024-08-13 23:39:54 -0700

    smallerNumbersThanCurrent

---
+++ 58.smallerNumbersThanCurrent.py
@@ -0,0 +1,8 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_arr = sorted(nums)
+        answer = [0] * len(nums)
+        for i in range(len(nums)):
+            answer[i] = sorted_arr.index(nums[i])
+        return answer
+        
